5dbb156659f180c46c40ec22c56b0232943bc435
jhalzel <jeremy123jeremy@gmail.com>
2023-01-30 18:02:40 -1000

    Adding button animations etc.

--- src/pages/home/Info.js
+++ src/pages/home/Info.js
@@ -4,20 +4,22 @@ import styles from "./Home.module.css"
 
 export default function Info() {
     const [showModal, setShowModal] = useState(false)
-    
+    const [button, setButton] = useState(true)
+
     const handleClick = () => {
         showModal ? setShowModal(false) : setShowModal(true)
     }
+
 
   return (
     <div>
         <button onClick={()=> setShowModal(!showModal)}>Info</button>
         {showModal && (
             <section className={styles.infosection}>
+                <button className={styles.innerButton} onClick={()=> setShowModal(false)}>x</button>
                 <div className={styles.infocontent}>
                     <h2>About</h2>
                     <p>This webpage is to be used to calculate and keep track of changes of your profits and margin for Amazon Sellers</p>
-                    <button className={styles.innerButton} onClick={()=> setShowModal(false)}>x</button>
                 </div>
             </section> 
         )}
