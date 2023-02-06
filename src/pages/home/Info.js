15053828a41900c1b8e1b3351e89ba7f828878ad
jhalzel <jeremy123jeremy@gmail.com>
2023-02-05 22:18:20 -0500

    Bug Fixes, CSS adjustments for better functions

--- src/pages/home/Info.js
+++ src/pages/home/Info.js
@@ -4,22 +4,31 @@ import styles from "./Home.module.css"
 
 export default function Info() {
     const [showModal, setShowModal] = useState(false)
-    const [button, setButton] = useState(true)
+    const [text, setText] = useState("Info")
 
     const handleClick = () => {
-        showModal ? setShowModal(false) : setShowModal(true)
+        // showModal ? setShowModal(false) : setShowModal(true)
+        if (showModal) {
+            setShowModal(false)
+            setText("Info")
+        }
+        else {
+            setShowModal(true)
+            setText('x')
+        }
     }
-
 
   return (
     <div>
-        <button onClick={()=> setShowModal(!showModal)}>Info</button>
+        <button onClick={()=>handleClick()}>{text}</button>
         {showModal && (
-            <section className={styles.infosection}>
-                <button className={styles.innerButton} onClick={()=> setShowModal(false)}>x</button>
-                <div className={styles.infocontent}>
-                    <h2>About</h2>
-                    <p>This webpage is to be used to calculate and keep track of changes of your profits and margin for Amazon Sellers</p>
+            <section>
+                <div>
+                    <h1>About</h1>
+                    <p>This webpage is to be used to calculate and keep track of changes of your profits and margin for Amazon Sellers
+                    To add a transaction, input the name of the product,the ASIN, and cost of the item on Amazon
+                When you submit that info a transaction card will pop up in the center of the screen
+                If you hove over the transaction card and click, there will be more input options for you to add more details dynamically as you discover costs of the item</p>
                 </div>
             </section> 
         )}
